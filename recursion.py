d841048c3054694f85ef325988f0c1a62bead79f
vagrant <vagrant@vagrant.vm>
2017-07-24 17:16:12 +0000

    tried binary numbers

--- recursion.py
+++ recursion.py
@@ -1,7 +1,7 @@
 def flatten_dict(bundle):
     """dict with nested dicts flatten so all values strings"""
     result = {}
-    if is_flat(bundle):
+    if is_flat_d(bundle):
         return bundle
     for item in bundle:
         if not isinstance(bundle[item], str):
@@ -12,11 +12,13 @@ def flatten_dict(bundle):
             result[item] = bundle[item]
     return result
 
+
 def is_flat_d(thing):
     for key, item in thing.iteritems():
         if not isinstance(item, str):
             return False
     return True
+
 
 def is_flat_l(thing):
     for item in thing:
@@ -24,8 +26,6 @@ def is_flat_l(thing):
             return False
     return True
 
-test = {6: {5: {3: '2'}}, 'a': 'b', 'c': 'd', 'e': {1: '2'}}
-test_list = [1,2,[3,4,5],7,[8,[90]]]
 
 def flatten_list(lst):
     if is_flat_l(lst):
@@ -44,11 +44,13 @@ def count_list(lst):
         return 0
     return 1 + count_list(lst[:-1])
 
+
 def print_list(lst):
     if not lst:
         return
     print lst[0]
     print_list(lst[1:])
+
 
 def get_index(lst, thing):
     count = 0
@@ -56,15 +58,18 @@ def get_index(lst, thing):
         return count
     return count + 1 + get_index(lst[1:], thing)
 
+
 def rev_string(word):
     if not word:
         return ""
     return word[-1] + rev_string(word[:-1])
 
+
 def sum_list(lst):
     if not lst:
         return 0
     return lst[0] + sum_list(lst[1:])
+
 
 def find_needle(needle, haystack):
     if not haystack:
@@ -75,3 +80,40 @@ def find_needle(needle, haystack):
     if result is not None:
         return 1 + result
     return None
+
+
+def bin(num):
+    if num < 2:
+        return str(num)
+    next = num % 2
+    return bin(num / 2) + str(next)
+
+
+def three(num):
+    if num < 3:
+        return str(num)
+    next = num % 3
+    return three(num / 3) + str(next)
+
+
+def rebase(num, base):
+    if num < base:
+        return str(num)
+    next = num % base
+    return rebase(num / base, base) + str(next)
+
+
+# the iterative solution to rebasing to decimal
+def to_ten(num, prev):
+    chars = list(num)
+    n = len(chars)
+    result = 0
+    for i in range(1, n + 1):
+        result += int(chars[i - 1]) * (int(prev) ** (n - i))
+    return result
+
+
+def tenbase(num, prev):
+    if len(num) == 0:
+        return 0
+    return int(num[-1]) + (tenbase(num[:-1], prev) * prev)
